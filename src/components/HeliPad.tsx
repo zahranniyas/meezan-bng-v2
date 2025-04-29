3b00e1805b6a885a5fd8ee32ede59fa0d34a1765
zahranniyas <zahranniyas@gmail.com>
2025-04-29 23:00:19 +0530

    fixed image reference error

--- src/components/HeliPad.tsx
+++ src/components/HeliPad.tsx
@@ -3,17 +3,13 @@ const HeliPad = () => {
     <div className="flex md:flex-row flex-col mt-36">
       <div className="md:w-[40%] w-full flex flex-col justify-center  items-center">
         <div className="p-10 flex flex-col items-center">
-          <img
-            src="/public/text-q-top.png"
-            alt="text-top"
-            className="w-[50%] mb-5"
-          />
+          <img src="/text-q-top.png" alt="text-top" className="w-[50%] mb-5" />
           <h1 className="text-center text-xl md:text-xl lg:text-3xl text-gray-700">
             Indulge in a breathtaking 40-minute helicopter ride that transforms
             your travel experience into an unforgettable memory
           </h1>
           <img
-            src="/public/text-q-bottom.png"
+            src="/text-q-bottom.png"
             alt="text-top"
             className="w-[50%] mt-5"
           />
